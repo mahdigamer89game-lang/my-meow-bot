b1ba4e2ebb324695d181cf74afd533dbc7a134ae
mahdigamer89game-lang <mahdigamer89game@gmail.com>
2026-09-19 02:52:38 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -396,8 +396,7 @@ async def main():
     async with app:
         await app.start()
         await app.updater.start_polling()
-        while True:
-            await asyncio.sleep(3600)
+        await app.updater.idle()
 
 if __name__ == '__main__':
-    asyncio.run(main()
+    asyncio.run(main())
